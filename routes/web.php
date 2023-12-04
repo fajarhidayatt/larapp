25c9eae29373917a2ae7cf7e347ea75671b37aa6
fajarhidayatt <fadjhar10@gmail.com>
2023-12-04 21:32:49 +0700

    create book controller and route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'title' => 'Home'
+    ]);
 });
+
+Route::resource('books', BookController::class);
